0f0de205897ac15cdf5cee1410d09feea0fd9706
Pawan Subedi <pawansubedi90558@gmail.com>
2025-03-02 23:16:41 +0545

    fixed bugs

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'NewsAggregator.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'NewsAggregator.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
